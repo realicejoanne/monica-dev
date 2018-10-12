5ced62640cad05e7fc6e21b0fb7fb92cb9fc8946
Raidiance <raihanakbar1999@gmail.com>
2018-10-12 19:00:10 +0700

    tambah activity dulu

---
+++ Monica/app/src/main/java/unpad/rockbottom/monica/SelectedCommitee.java
@@ -0,0 +1,13 @@
+package unpad.rockbottom.monica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectedCommitee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_commitee);
+    }
+}
