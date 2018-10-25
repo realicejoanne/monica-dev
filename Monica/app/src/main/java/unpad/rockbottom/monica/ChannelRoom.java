136b033916ed4b2da226d9ed03e296250ec44474
Patricia Joanne <alicetheuglyduckling@gmail.com>
2018-10-25 11:47:09 +0700

    menghias home

--- Monica/app/src/main/java/unpad/rockbottom/monica/ChannelRoom.java
+++ Monica/app/src/main/java/unpad/rockbottom/monica/ChannelRoom.java
@@ -2,13 +2,12 @@ package unpad.rockbottom.monica;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
-public class CreateCommittee extends AppCompatActivity {
+public class ChannelRoom extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_committee);
+        setContentView(R.layout.activity_channel_room);
     }
 }
