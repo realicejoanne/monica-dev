33ab5fb20650b7866f9e65d8c0c83676784bf3c9
Raidiance <raihanakbar1999@gmail.com>
2018-10-28 01:30:37 +0700

    Baru buat activity doang

--- Monica/app/src/main/java/unpad/rockbottom/monica/Chat.java
+++ Monica/app/src/main/java/unpad/rockbottom/monica/Chat.java
@@ -1,7 +1,10 @@
 package unpad.rockbottom.monica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Chat extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class Chat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+    }
+
+    public void onClickChatRoom (View view){
+        try {
+            Intent i = new Intent(Chat.this, ChatRoom.class);
+            startActivity(i);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(getApplication(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
