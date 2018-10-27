1ea0aa84b99084114b296fb10f8c4927703e301c
Raidiance <raihanakbar1999@gmail.com>
2018-10-27 23:30:18 +0700

    Resolve error on Anjass's pull

--- Monica/app/src/main/java/unpad/rockbottom/monica/FragmentHome.java
+++ Monica/app/src/main/java/unpad/rockbottom/monica/FragmentHome.java
@@ -1,6 +1,8 @@
 package unpad.rockbottom.monica;
 
 import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,12 +10,49 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.annotation.Nullable;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class FragmentHome extends Fragment {
+    private LinearLayout linear_cbs_btn;
+    private ImageButton cbs_btn;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        cbs_btn = (ImageButton) view.findViewById(R.id.cbs_btn);
+        cbs_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent i = new Intent(getActivity(), CommitteeView.class);
+                    startActivity(i);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        linear_cbs_btn = (LinearLayout) view.findViewById(R.id.cm_cbs);
+        linear_cbs_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent i = new Intent(getActivity(), CommitteeView.class);
+                    startActivity(i);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
         return view;
     }
