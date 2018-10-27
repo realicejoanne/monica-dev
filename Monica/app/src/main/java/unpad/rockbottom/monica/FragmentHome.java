e24e04d77e7b5dfae081bb15282450782a362e4f
Patricia Joanne <alicetheuglyduckling@gmail.com>
2018-10-27 23:05:48 +0700

    home, profile, settings

--- Monica/app/src/main/java/unpad/rockbottom/monica/FragmentHome.java
+++ Monica/app/src/main/java/unpad/rockbottom/monica/FragmentHome.java
@@ -8,6 +8,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class FragmentHome extends Fragment {
     @Nullable
@@ -18,3 +29,4 @@ public class FragmentHome extends Fragment {
         return view;
     }
 }
+
--- Monica/app/src/main/java/unpad/rockbottom/monica/FragmentProfile.java
+++ Monica/app/src/main/java/unpad/rockbottom/monica/FragmentProfile.java
@@ -8,8 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.annotation.Nullable;
+import android.widget.Toolbar;
 
 public class FragmentProfile extends Fragment {
+
+    Toolbar mActionBarToolbar;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
