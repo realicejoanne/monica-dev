ae1e43a3be2af30a7ff36f5ef02a360ad9cc87cc
Raidiance <raihanakbar1999@gmail.com>
2018-10-28 16:38:11 +0700

    Call room lumayan

--- Monica/app/src/main/java/unpad/rockbottom/monica/Call.java
+++ Monica/app/src/main/java/unpad/rockbottom/monica/Call.java
@@ -1,7 +1,10 @@
 package unpad.rockbottom.monica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Call extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class Call extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call);
+    }
+
+    public void onClickCallRoom (View view){
+        try {
+            Intent i = new Intent(Call.this, CallRoom.class);
+            startActivity(i);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(getApplication(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- Monica/app/src/main/java/unpad/rockbottom/monica/CallRoom.java
+++ Monica/app/src/main/java/unpad/rockbottom/monica/CallRoom.java
@@ -1,13 +1,21 @@
 package unpad.rockbottom.monica;
 
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class CallRoom extends AppCompatActivity {
+
+    private ImageButton speak_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_room);
+
+        speak_btn = (ImageButton) findViewById(R.id.speak_btn);
     }
 }
