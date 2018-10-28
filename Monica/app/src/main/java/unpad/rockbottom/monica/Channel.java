d9ae8da3b9e6ea116c9a4d07c16d508ef50388f4
Raidiance <raihanakbar1999@gmail.com>
2018-10-28 22:05:01 +0700

    Jadi channelRoom viewnya. copas dari chatRoom

--- Monica/app/src/main/java/unpad/rockbottom/monica/Channel.java
+++ Monica/app/src/main/java/unpad/rockbottom/monica/Channel.java
@@ -1,7 +1,10 @@
 package unpad.rockbottom.monica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Channel extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class Channel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_channel);
+    }
+
+    public void onClickChannelRoom (View view){
+        try {
+            Intent i = new Intent(Channel.this, ChannelRoom.class);
+            startActivity(i);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(getApplication(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- Monica/app/src/main/java/unpad/rockbottom/monica/ChannelRoom.java
+++ Monica/app/src/main/java/unpad/rockbottom/monica/ChannelRoom.java
@@ -8,6 +8,6 @@ public class ChannelRoom extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_call_room);
+        setContentView(R.layout.activity_channel_room);
     }
 }
