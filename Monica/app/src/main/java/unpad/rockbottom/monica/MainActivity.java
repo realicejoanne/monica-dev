37e62d47647add54d989241aab5dd79e36248626
Patricia Joanne <alicetheuglyduckling@gmail.com>
2018-10-28 14:52:25 +0700

    appbar, drawer, font

--- Monica/app/src/main/java/unpad/rockbottom/monica/MainActivity.java
+++ Monica/app/src/main/java/unpad/rockbottom/monica/MainActivity.java
@@ -1,6 +1,8 @@
 package unpad.rockbottom.monica;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,11 +13,17 @@ import android.support.v4.app.Fragment;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout sDrawerLayout;
+    private ActionBarDrawerToggle sToogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        sToogle = new ActionBarDrawerToggle(this,sDrawerLayout,R.string.open,R.string.close);
+        sDrawerLayout.addDrawerListener(sToogle);
+        sToogle.syncState();
 
         BottomNavigationView bottomNav = findViewById(R.id.bottomnav);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
@@ -23,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentHome()).commit();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        if(sToogle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
