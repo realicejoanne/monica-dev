8d11c82b80b2399763d83d1c5b39ca3256c62b84
faizinahsan <faizin110998@gmail.com>
2018-10-28 23:39:08 +0700

    add a pop up finish

--- Monica/app/src/main/java/unpad/rockbottom/monica/FragmentHome.java
+++ Monica/app/src/main/java/unpad/rockbottom/monica/FragmentHome.java
@@ -1,6 +1,7 @@
 package unpad.rockbottom.monica;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,6 +15,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
@@ -21,11 +23,18 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class FragmentHome extends Fragment {
+    ImageButton homeToCommittee;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-
+        homeToCommittee = (ImageButton) view.findViewById(R.id.home_to_committee);
+        homeToCommittee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(),CommitteeView.class));
+            }
+        });
         return view;
     }
 }
